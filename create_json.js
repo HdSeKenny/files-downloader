aa2fb91a1fae22df1fadb8ae45f4ef5378162c2d
Kenny <cnkuan@qq.com>
2017-04-26 10:27:19 +0800

    create json

---
+++ create_json.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+
+const sweetsObj = require('./sweets.json');
+
+const sweets = sweetsObj.sweets;
+const exampleSweets = [];
+const exampleUsers = [];
+const readFilePromises = [];
+
+
+const readFiles = (dirname, callback) => {
+  fs.readdir(dirname, (err, filenames) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+
+    callback(filenames);
+    // filenames.forEach((filename) => {
+    //   readFilePromises.push(filePromise(dirname, filename));
+    // });
+    // Promise.all(readFilePromises).then(() => {
+
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
+  });
+};
+
+const getfileName = (string) => {
+  const pointIndex = string.indexOf('.');
+  if (pointIndex < 1) {
+    return '';
+  }
+  return pointIndex < 1 ? '' : string.substring(0, pointIndex);
+};
+
+const convertUserObject = (sweet)  => {
+  const usrStrId = sweet.user.id_str;
+  const fileNames = [];
+  readFiles('images/', (filenames) => {
+    filenames.forEach(stringName => {
+      const fileName = getfileName(stringName);
+      if (fileName) {
+
+      }
+    });
+  });
+};
+
+convertUserObject();
+
+// sweets.forEach((sweet, index) => {
+//   convertUserObject();
+// });
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
+/* eslint-disable all, no-param-reassign */
+const mongodb = require('mongodb');
 const fs = require('fs');
 
 const download = require('./download');
 
-const data = { sweets: [], user_images:[] };
+const ObjectID = mongodb.ObjectID;
+const data = { sweets: [], users: [] };
 const readFilePromises = [];
 
-const filePromise = (dirname, filename) => new Promise ((resolve, reject) => {
+const filePromise = (dirname, filename) => new Promise((resolve, reject) => {
   fs.readFile(dirname + filename, 'utf-8', (err, content) => {
     if (err) {
       reject(err);
@@ -15,7 +17,15 @@ const filePromise = (dirname, filename) => new Promise ((resolve, reject) => {
     sweets.forEach(sweet => {
       const pointIndex = filename.indexOf('.');
       sweet.tag = filename.substring(0, pointIndex);
+      sweet.type = 'moments';
+      sweet._id = ObjectID(sweet.id_str);
+      sweet.show_comments = false;
+      sweet.likers = [];
+      sweet.comments = [];
+      sweet.author = ObjectID(sweet.user.id_str);
+      sweet.user._id = ObjectID(sweet.user.id_str);
       data.sweets.push(sweet);
+      data.users.push(sweet.user);
     });
     resolve();
   });
@@ -30,87 +40,73 @@ function readFiles(dirname) {
     filenames.forEach((filename) => {
       readFilePromises.push(filePromise(dirname, filename));
     });
-    Promise.all(readFilePromises).then(() => {
-      const sweetsJson = JSON.stringify(data, null, 2);
-      fs.writeFile('sweets.json', sweetsJson, 'utf8', () => {
-        const downloadPromises = [];
-        data.sweets.forEach((sweet, index) => {
-          if (index > 30) {
-            return;
-          }
-          const { user } = sweet;
-          const userImagesObject = {
-            id: user.id,
-            id_str: user.id_str
-          };
-
-          if (user.profile_background_image_url) {
-            const fileFormat = getFileFormatByUrl(user.profile_background_image_url);
-            const download_image_url = `images/${user.id_str}${fileFormat}`;
-            downloadPromises.push(download(user.profile_background_image_url, download_image_url));
-            userImagesObject.profile_background_image_url = download_image_url;
-          }
-
-          if (user.profile_background_image_url_https) {
-            const fileFormat = getFileFormatByUrl(user.profile_background_image_url_https);
-            const download_image_url = `images/${user.id_str}${fileFormat}`;
-            downloadPromises.push(download(user.profile_background_image_url_https, download_image_url));
-            userImagesObject.profile_background_image_url_https = download_image_url;
-          }
-
-          if (user.profile_image_url) {
-            const fileFormat = getFileFormatByUrl(user.profile_image_url);
-            const download_image_url = `images/${user.id_str}${fileFormat}`;
-            downloadPromises.push(download(user.profile_image_url, download_image_url));
-            userImagesObject.profile_image_url = download_image_url;
-          }
-
-          if (user.profile_image_url_https) {
-            const fileFormat = getFileFormatByUrl(user.profile_image_url_https);
-            const download_image_url = `images/${user.id_str}${fileFormat}`;
-            downloadPromises.push(download(user.profile_image_url_https, download_image_url));
-            userImagesObject.profile_image_url_https = download_image_url;
-          }
-
-          if (user.profile_banner_url) {
-            const fileFormat = getFileFormatByUrl(user.profile_banner_url);
-            const download_image_url = `images/${user.id_str}${fileFormat}`;
-            downloadPromises.push(download(user.profile_banner_url, download_image_url));
-            userImagesObject.profile_banner_url = download_image_url;
-          }
-
-          data.user_images.push(userImagesObject);
+    Promise.all(readFilePromises)
+      .then(() => {
+        const sweetsJson = JSON.stringify(data, null, 2);
+        fs.writeFile('sweets.json', sweetsJson, 'utf8', () => {
+          console.log('\ndone...');
         });
 
-         console.log(JSON.stringify(data.user_images, null, 2));
+        // const downloadPromises = [];
+        // data.sweets.forEach((sweet, index) => {
+        //   const { user } = sweet;
+        //   const userImagesObject = {
+        //     id: user.id,
+        //     id_str: user.id_str
+        //   };
 
-        Promise.all(downloadPromises).then(() => {
-          console.log(data.user_images.length);
-          console.log('\n\ndone')
-        })
-        .catch((error) => {
-          console.log(error)
-        })
+        // if (user.profile_background_image_url) {
+        //   const fileFormat = getFileFormatByUrl(user.profile_background_image_url);
+        //   const download_image_url = `images/${user.id_str}${fileFormat}`;
+        //   downloadPromises.push(download(user.profile_background_image_url, download_image_url));
+        //   userImagesObject.profile_background_image_url = download_image_url;
+        // }
+
+        // if (user.profile_background_image_url_https) {
+        //   const fileFormat = getFileFormatByUrl(user.profile_background_image_url_https);
+        //   const download_image_url = `images/${user.id_str}${fileFormat}`;
+        //   downloadPromises.push(download(user.profile_background_image_url_https, download_image_url));
+        //   userImagesObject.profile_background_image_url_https = download_image_url;
+        // }
+
+        // if (user.profile_image_url) {
+        //   const fileFormat = getFileFormatByUrl(user.profile_image_url);
+        //   const download_image_url = `images/${user.id_str}${fileFormat}`;
+        //   downloadPromises.push(download(user.profile_image_url, download_image_url));
+        //   userImagesObject.profile_image_url = download_image_url;
+        // }
+
+        // if (user.profile_image_url_https) {
+        //   const fileFormat = getFileFormatByUrl(user.profile_image_url_https);
+        //   const download_image_url = `images/${user.id_str}${fileFormat}`;
+        //   downloadPromises.push(download(user.profile_image_url_https, download_image_url));
+        //   userImagesObject.profile_image_url_https = download_image_url;
+        // }
+
+        // data.user_images.push(userImagesObject);
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+    // Promise.all(downloadPromises).then(() => {
+
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
   });
 }
 
-function getFileFormatByUrl(url) {
-  let format = '.png';
-  const lastPointIndex = url.lastIndexOf('.');
-  const lastSlashIndex =  url.lastIndexOf('/');
-  if (lastPointIndex && lastSlashIndex && lastSlashIndex < lastPointIndex) {
-    format = url.substring(lastPointIndex);
-  }
+// function getFileFormatByUrl(url) {
+//   let format = '.png';
+//   const lastPointIndex = url.lastIndexOf('.');
+//   const lastSlashIndex = url.lastIndexOf('/');
+//   if (lastPointIndex && lastSlashIndex && lastSlashIndex < lastPointIndex) {
+//     format = url.substring(lastPointIndex);
+//   }
 
-  return format;
-}
+//   return format;
+// }
 
 
 readFiles('json/');
-
-
